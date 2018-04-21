f64eab075f5f1d86def191e594b8167cffbabf8d
sherwinxie <sherwinxie@foxmail.com>
2018-04-22 00:30:22 +0800

    随笔
    看Hamlet有感而发 好像该叫随码..

---
+++ Explorer.py
@@ -0,0 +1,12 @@
+# Created: 21th Apr 2018 by Sherwin X
+# Reference: To be, or not to be that is the question. ——Hamlet
+# Description: watch the movie，feeling code, just for fun..
+toExplore = input("'toExplore' Your choice(Y/N):").upper() == "Y"
+NULL = False
+while toExplore or NULL:
+    print("that is the question.")
+print("No question any more!")
+
+
+
+
