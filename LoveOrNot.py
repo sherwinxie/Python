d2fa433b56a4e41e51fddaa2e36ce23ac27074bf
sherwinxie <sherwinxie@foxmail.com>
2018-04-22 22:21:10 +0800

    随码
    我是不是在用py写诗..

---
+++ LoveOrNot.py
@@ -0,0 +1,18 @@
+# Created: 22th April 2018 by Sherwin X
+# Reference: 《见与不见》  Meet or Not meet ？
+# Description: 无论你爱与不爱我 我的爱就在那里，不增不减.. 
+me = input("Do you love me？(Y/N): ").lower()
+class You:
+    @staticmethod
+    def love(is_me):
+      return 1 if (is_me == "y") else 0
+# Whether you love me or not, my love is still there, neither growing nor fading.
+if(You.love(me) == 1 or You.love(me) == 0 ):
+    myLove = 1
+    ++myLove
+    --myLove
+    print("myLove always "+str(myLove))
+
+
+
+
